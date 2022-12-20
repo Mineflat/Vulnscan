4290946ef8baa5c244a948b462ffdacbaa4df29a
Mineflat <mineflat@yandex.ru>
2022-12-20 22:54:32 +0300

    Добавил папку с модулями и описание внутри

--- AutoscanBot/Configuration.cs
+++ AutoscanBot/Configuration.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AutoscanBot
+﻿namespace AutoscanBot
 {
     internal class Configuration
     {
